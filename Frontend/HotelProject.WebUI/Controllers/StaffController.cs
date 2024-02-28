d7e4eac6a76603d5790aecda9463c915cfafc949
habibesayrac <habibesayrac@outlook.com>
2024-02-28 09:39:47 +0300

    DeleteStaff added

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -45,5 +45,15 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
+      public async Task<IActionResult> DeleteStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"http://localhost:30055/api/Staff?StaffID={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
