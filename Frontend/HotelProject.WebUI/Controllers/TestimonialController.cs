bb88fa2a48d845d519fcb6c7d99ebf45756c7694
habibesayrac <habibesayrac@outlook.com>
2024-02-29 10:38:37 +0300

    testimonialController added

---
+++ Frontend/HotelProject.WebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.WebUI.Models.Staff;
+using HotelProject.WebUI.Models.Testimonial;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:30055/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<TestimonialViewModel>>(jsonData);
+                return View(values);
+            }
+            return View();
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddTestimonial(TestimonialViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject()
+            return View();
+        }
+    }
+}
