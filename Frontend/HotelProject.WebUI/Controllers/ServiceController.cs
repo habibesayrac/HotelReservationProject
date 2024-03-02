35edde9272ef843e0972da19e169bfea75455b46
habibesayrac <habibesayrac@outlook.com>
2024-03-02 16:00:45 +0300

    ServiceDto Added

---
+++ Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,56 @@
+﻿using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:30055/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddService()
+        {
+          return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddService(CreateServiceDto serviceDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(serviceDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("http://localhost:30055/api/Service",stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+      
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            return View();
+        }
+    }
+}
