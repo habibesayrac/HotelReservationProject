7bf9aba0ad42bfa0aecc427f37852fd74956c1cd
habibesayrac <habibesayrac@outlook.com>
2024-03-10 14:42:07 +0300

    BookingApi Added

---
+++ RapidApi/RapidApi.Consume/Controllers/ExchangeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApi.Consume.Models;
+
+namespace RapidApi.Consume.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "e32d0223f5msh90bdc26a183e3c0p11dca3jsn0190e2427676" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+
+            }
+        }
+    }
+}
