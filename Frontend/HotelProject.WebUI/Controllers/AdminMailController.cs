3b84000010883a88694f1ef7a8c7aa012665d8bf
habibesayrac <habibesayrac@outlook.com>
2024-03-20 22:55:24 +0300

    Mail send and pass key added

--- Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -16,7 +16,7 @@ namespace HotelProject.WebUI.Controllers
         public IActionResult Index(AdminMailViewModel model)
         {
             MimeMessage mimeMessage = new MimeMessage();
-            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "burası bizim mailmiz");
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "hsayracode@gmail.com");
             mimeMessage.From.Add(mailboxAddressFrom);
 
             MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
@@ -30,11 +30,14 @@ namespace HotelProject.WebUI.Controllers
 
             SmtpClient client = new SmtpClient();
             client.Connect("smtp.gmail.com", 587, false);
-            client.Authenticate("mail", "password key");
+            client.Authenticate("hsayracode@gmail.com", "wiobaeobfwlauwva");
             client.Send(mimeMessage);
             client.Disconnect(true);
 
+            //Gönderilen mailin veri tabanına kaydedilmesi
+
             return View();
         }
+
     }
 }
