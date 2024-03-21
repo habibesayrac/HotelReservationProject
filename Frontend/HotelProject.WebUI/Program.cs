c478002132648ef8c27a4a02e099de874199d120
habibesayrac <habibesayrac@outlook.com>
2024-03-21 09:32:03 +0300

    404 page added

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -42,6 +42,8 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404","?code={0}");
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseAuthentication();
