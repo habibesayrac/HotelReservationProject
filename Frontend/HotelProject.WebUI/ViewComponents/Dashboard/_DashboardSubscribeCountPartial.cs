45e8824098cdf87385cff43d51679209f6a93641
habibesayrac <habibesayrac@outlook.com>
2024-03-31 17:27:07 +0300

    twitter&linkedin added

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -26,8 +26,51 @@ namespace HotelProject.WebUI.ViewComponents.Dashboard
                 ResultInstagramFollowersDto resultInstagramFollowersDtos = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
                 ViewBag.v1 = resultInstagramFollowersDtos.followers;
                 ViewBag.v2 = resultInstagramFollowersDtos.following;
-                return View(resultInstagramFollowersDtos);
             }
+
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://twitter32.p.rapidapi.com/getProfile?username=tarkan"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "e32d0223f5msh90bdc26a183e3c0p11dca3jsn0190e2427676" },
+        { "X-RapidAPI-Host", "twitter32.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                ResultTwitterFollowersDto resultTwitterFollowersDtos = JsonConvert.DeserializeObject<ResultTwitterFollowersDto>(body2);
+                ViewBag.v3 = resultTwitterFollowersDtos.data.user_info.followers_count;
+                ViewBag.v4 = resultTwitterFollowersDtos.data.user_info.friends_count;
+                
+            }
+
+ 
+            var client3 = new HttpClient();
+            var request3 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile?linkedin_url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fmuratyucedag%2F&include_skills=false"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "e32d0223f5msh90bdc26a183e3c0p11dca3jsn0190e2427676" },
+        { "X-RapidAPI-Host", "fresh-linkedin-profile-data.p.rapidapi.com" },
+    },
+            };
+            using (var response3 = await client3.SendAsync(request3))
+            {
+                response3.EnsureSuccessStatusCode();
+                var body3 = await response3.Content.ReadAsStringAsync();
+                ResultLinkedinFollowersDto resultLinkedinFollowersDtos = JsonConvert.DeserializeObject<ResultLinkedinFollowersDto>(body3);
+                ViewBag.v5 = resultLinkedinFollowersDtos.data.followers_count;
+            }
+
+            return View();
+
         }
     }
 }
